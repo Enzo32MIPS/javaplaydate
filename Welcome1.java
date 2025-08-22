d8fb40b7c7faea179d8bd732f1718f53f19b0e3b
Enzo32MIPS <enzomoraes.aluno@unipampa.edu.br>
2025-08-22 09:01:26 -0300

    fixed it

--- Welcome1.java
+++ Welcome1.java
@@ -1,41 +1,50 @@
 import java.util.Scanner;
 
-public class Welcome1{		// método principal inicia a execução do aplicativo Java
+public class Welcome1{
 
 	public static void main( String args[] ){
 
-	System.out.println( "Welcome to Java Programming!" );
-	int opt = 1;
-	int saldo = 5000;
-	do{
-	System.out.println("======BANCO======");
-	Scanner input  = new Scanner(System.in);
-	System.out.pŕintln("1. Ver dinheiros");
-	System.out.println("2. Depositar na conta");
-	System.out.println("3. Fazer uma compra");
-	opt = input.nextInt();
+		System.out.println( "Welcome to Java Programming!" );
+		int opt = 1;
+		int saldo = 5000;
+		do{
+			if(saldo <= 0){
+				opt = 0;
+			}
+			System.out.println("======BANCO======");
+			Scanner input = new Scanner(System.in);
+			System.out.println("1. Ver dinheiros");
+			System.out.println("2. Depositar na conta");
+			System.out.println("3. Fazer uma compra");
+			opt = input.nextInt();
 	
-	switch(opt){
+			switch(opt){
 
-	case 1:
-	System.out.println("Saldo: "+saldo);
-	break;
+				case 1:
+				System.out.println("Saldo: "+saldo);
+				break;
 
-	case 2:
-	System.out.println("Digite o valor a ser somado: ");
-	int dep = input.nextInt();
-	saldo += dep;
-	break;
+				case 2:
+				System.out.println("Digite o valor a ser somado: ");
+				int dep = input.nextInt();
+				saldo += dep;
+				break;
 
-	default:
-	System.out.println("Erro!");
-	break;
+				case 3:
+				System.out.println("Digite o valor a ser subtraido: ");
+				int comp = input.nextInt();
+				saldo -= comp;
+				break;
 
+				case 0:
+				System.out.println("Saindo...");
+				break;
 
+				default:
+				System.out.println("Erro!");
+				break;
 
-
-
+			}
+		}while (opt != 0);
+	}
 }
-	}while (opt != 0);
-	}		// fim do método principal
-}		// fim da classe Welcome1
